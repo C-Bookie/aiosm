02c0a7fd04fba0daefbf94561b945b17382c9bec
Callum B-C <duck-vader@hotmail.co.uk>
2022-01-16 13:54:55 +0000

    bump to 0.0.3

--- aiosm/__init__.py
+++ aiosm/__init__.py
@@ -1,4 +1,13 @@
-__version__ = '0.0.2'
+"""An asynchronous TCP connection hub
+
+todo:
+    implement heartbeat
+    implement inheritable class structure for client side to represent a node with header functions for RPC
+    support loop.create_datagram_endpoint()
+
+"""
+
+__version__ = '0.0.3'
 
 from .__main__ import run_host
 from .radio import Radio
