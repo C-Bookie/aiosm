480668f0ce014c4b8ccd911df8d42fa0973abed9
callum.burns-curd <callum.burns-curd@nanoporetech.com>
2023-01-09 23:42:31 +0000

    Revert "Add filter to dprint"
    This reverts commit 609b47ed230709d2f5ca1b03db17816f34de19dc.

--- aiosm/dprint.py
+++ aiosm/dprint.py
@@ -4,11 +4,4 @@ import datetime
 
 
 def dprint(message_type: str, *args) -> None:
-	# todo, consider if this filtering is needed
-	if message_type in [
-		"Sending",
-		"Received",
-	]:
-		print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + "")
-	else:
-		print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + ": \n\t", *args)
+	print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + ": \n\t", *args)
