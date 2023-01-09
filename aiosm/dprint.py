609b47ed230709d2f5ca1b03db17816f34de19dc
callum.burns-curd <callum.burns-curd@nanoporetech.com>
2023-01-09 23:35:24 +0000

    Add filter to dprint

--- aiosm/dprint.py
+++ aiosm/dprint.py
@@ -4,4 +4,11 @@ import datetime
 
 
 def dprint(message_type: str, *args) -> None:
-	print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + ": \n\t", *args)
+	# todo, consider if this filtering is needed
+	if message_type in [
+		"Sending",
+		"Received",
+	]:
+		print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + "")
+	else:
+		print(str(datetime.datetime.now()) + ": (" + asyncio.current_task().get_name() + ") " + message_type + ": \n\t", *args)
